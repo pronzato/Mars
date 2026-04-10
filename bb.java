4e4f4885199cf46a301dc08b8c48950b2fb0ebb3
pronzato <pronzato@gmail.com>
2026-04-10 07:51:19 -0400

    Update bb.java

--- bb.java
+++ bb.java
@@ -1,8 +1,1 @@
-{
-            "type": "java",
-            "name": "StitchStudioApp",
-            "request": "launch",
-            "mainClass": "org.pronzato.fabric.stitch.studio.StitchStudioApp",
-            "projectName": "fabric-lite-stitch-studio",
-            "vmArgs": "--add-opens=java.base/java.nio=org.apache.arrow.memory.core,ALL-UNNAMED -Dfabric.group=platform -Dfabric.name=studio -Dfabric.instance=instance1-${env:USERNAME} -Dfabric.discoveryUrls=127.0.0.1:9507 -Dfabric.kerberosKeytabPath=C:/dev/fabric/vault/kerberos/dfabric.keytab -Dfabric.kerberosUsername=dfabric -Dfabric.secretKeyFile=C:/dev/fabric/vault/keys/platform/platform.fabrickey -Dfabric.tlsTrustCertPath=C:/dev/fabric/vault/tls/demo-ca.pem -Dfabric.port=8080 -Dfabric.stitch.runtime.configInstance=instance1"
-        }
+null
